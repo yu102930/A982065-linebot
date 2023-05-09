2d80b32c36340417af1aa0ee6b7ecd8f3d7b4633
lamb <yany57228@gmail.com>
2023-05-09 15:33:23 +0800

    init

---
+++ A982065-linebot/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace A982065_linebot.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
