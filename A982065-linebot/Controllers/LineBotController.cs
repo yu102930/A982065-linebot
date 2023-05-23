83ed1c742bcd690d5663188bcb47c851bb9e21b4
lamb <yany57228@gmail.com>
2023-05-23 16:55:36 +0800

    新增機器人相關設定

---
+++ A982065-linebot/Controllers/LineBotController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Line.Messaging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace A982065_linebot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LineBotController : ControllerBase
+    {
+        
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly HttpContext _httpContext;
+        private readonly LineBotConfig _lineBotConfig;
+        
+        public LineBotController(IServiceProvider serviceProvider)
+        {
+            _httpContextAccessor = serviceProvider.GetRequiredService<IHttpContextAccessor>();
+            _httpContext = _httpContextAccessor.HttpContext;
+            _lineBotConfig = new LineBotConfig();
+            _lineBotConfig.channelSecret = "46928f36db19a80a45bc77e3bf62d4ee";
+            _lineBotConfig.accessToken = "XlrFehlxoRfl/XUnKJZ7Q0pcLHxwvKiXeGq/WkB6tAsH1lXasEdDzX6mvLiqeM8zYqfLBx42nRH9IQ/AWPxtYromxtsWNeEF+hhUYT63mbVbZgqg0fGjf0mXp3b5BTRMYlS2AiU5t8Md3IwUI66GGwdB04t89/1O/w1cDnyilFU=";
+        }
+        
+        //完整的路由網址就是 https://xxx/api/linebot/run
+        [HttpPost ("run")]
+        public async Task<IActionResult> Post()
+        {
+            try
+            {
+                var events = await _httpContext.Request.GetWebhookEventsAsync(_lineBotConfig.channelSecret);
+                var lineMessagingClient = new LineMessagingClient(_lineBotConfig.accessToken);
+                var lineBotApp = new LineBotApp(lineMessagingClient);
+                await lineBotApp.RunAsync(events);
+            }
+            catch (Exception ex)
+            {
+                //需要 Log 可自行加入
+                //_logger.LogError(JsonConvert.SerializeObject(ex));
+            }
+            return Ok();
+        }
+    }
+}
---
+++ A982065-linebot/LineBotConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace A982065_linebot;
+
+public class LineBotConfig
+{
+    public string accessToken { get; set; }
+    public string channelSecret{ get; set; }
+}
