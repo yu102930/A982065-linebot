5693093a44866f5e655b05b74a252f71dc05f614
lamb <yany57228@gmail.com>
2023-05-30 16:02:02 +0800

    新增阿比地獄樂園

--- A982065-linebot/LineBotApp.cs
+++ A982065-linebot/LineBotApp.cs
@@ -6,6 +6,8 @@ namespace A982065_linebot;
 public class LineBotApp : WebhookApplication
 {
     private readonly LineMessagingClient _messagingClient;
+
+    private static Dictionary<string, string> _pool = new Dictionary<string, string>();
     public LineBotApp(LineMessagingClient lineMessagingClient)
     {
         _messagingClient = lineMessagingClient;
@@ -24,18 +26,103 @@ public class LineBotApp : WebhookApplication
                 var channelId = ev.Source.Id;
                 //使用者Id
                 var userId = ev.Source.UserId;
+                //使用者輸入的文字
+                var text = ((TextEventMessage)ev.Message).Text;
+
+                if (PoolHasMsg(text))
+                {
+                    // 從記憶體池查詢資料
+                    string response = GetResponse(text);
+                    result = new List<ISendMessage>
+                    {
+                        new TextMessage(response)
+                    };
+                }
+                else
+                {
+                    if (CheckFormat(text))
+                    {
+                        //將資料寫入記憶體池
+                        TeachDog(text);
+                    }
+                }
                 
-                 var  text =((TextEventMessage)ev.Message).Text; 
+                /*
                 //回傳 hellow
                 result = new List<ISendMessage>
                 {
                     new TextMessage("你家有貓嗎?"+ text  )
                 };
+                */
             }
                 break;
         }
 
         if (result != null)
             await _messagingClient.ReplyMessageAsync(ev.ReplyToken, result);
+    }
+
+    /// <summary>
+    /// 確認是否已經學習過這個對話
+    /// </summary>
+    /// <param name="inputMsg"></param>
+    /// <returns></returns>
+    private bool PoolHasMsg(string inputMsg)
+    {
+        return _pool.ContainsKey(inputMsg);
+    }
+ 
+    /// <summary>
+    /// 用於 已經學習過的對話
+    /// </summary>
+    /// <param name="inputMsg"></param>
+    /// <returns></returns>
+    private string GetResponse(string inputMsg)
+    {
+        return _pool[inputMsg];
+    }
+ 
+    private bool CheckFormat(string inputMsg)
+    {
+        bool result = false;
+        try
+        {
+            string[] subs = inputMsg.Split(';');
+            //檢查
+            if (subs.Length == 3)
+            {
+                if (subs[0] == "阿比地獄樂園")
+                {
+                    result = true;
+                }
+            }
+        }
+        catch (Exception exp)
+        {
+            Console.WriteLine(exp);
+            throw;
+        }
+        return result;
+    }
+ 
+    private void TeachDog(string inputMsg)
+    {
+        try
+        {
+            string[] subs = inputMsg.Split(';');
+            //檢查
+            if (subs.Length == 3)
+            {
+                if (subs[0] == "阿比地獄樂園")
+                {
+                    _pool.Add(subs[1],subs[2]);
+                }
+            }
+        }
+        catch (Exception exp)
+        {
+            Console.WriteLine(exp);
+            throw;
+        }
     }
 }
